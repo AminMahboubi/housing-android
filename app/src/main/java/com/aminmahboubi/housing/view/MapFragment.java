3352a9c3a7116797a74abf6ef850fcc2826f1ae8
Amin Mahboubi <aminmahboobi@gmail.com>
2018-02-07 00:46:00 +0100

    MapActivity onResume Fix

--- app/src/main/java/com/aminmahboubi/housing/view/MapFragment.java
+++ app/src/main/java/com/aminmahboubi/housing/view/MapFragment.java
@@ -6,17 +6,13 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
-import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,36 +21,27 @@ import android.widget.Toast;
 import com.aminmahboubi.housing.HouseActivity;
 import com.aminmahboubi.housing.R;
 import com.aminmahboubi.housing.model.House;
-import com.aminmahboubi.housing.util.PermissionUtils;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.Circle;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
 
-public class MapFragment extends Fragment{
+public class MapFragment extends Fragment {
 
     private static final String HOUSE = "house";
-    /**
-     * Request code for location permission request.
-     *
-     * @see #onRequestPermissionsResult(int, String[], int[])
-     */
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
     private SupportMapFragment mapFragment;
     private GoogleMap mMap;
     private ArrayList<House> houses;
 
-    public MapFragment() {
-    }
+    public MapFragment() {}
 
     public static MapFragment newInstance(ArrayList<House> houses) {
         MapFragment fragment = new MapFragment();
@@ -74,45 +61,46 @@ public class MapFragment extends Fragment{
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
-
         if (mapFragment == null) {
             mapFragment = SupportMapFragment.newInstance();
-
-            mapFragment.getMapAsync(googleMap -> {
-                mMap = googleMap;
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(45.46, 9.19), 11));
-                mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter(getActivity()));
-
-                enableMyLocationIfPermitted();
-                mMap.setOnMyLocationButtonClickListener(() -> {
-                    mMap.moveCamera(CameraUpdateFactory.zoomBy(13));
-                    return false;
-                });
-
-                BitmapDescriptor markerIcon = getBitmapFromVectorDrawable(getContext(), R.drawable.ic_place_black_24dp);
-
-                for (House house : houses) {
-                    mMap.addMarker(new MarkerOptions().position(house.getLatLng()).icon(markerIcon)).setTag(house);
-                }
-
-                mMap.setOnInfoWindowClickListener(marker -> {
-                    House house = (House) marker.getTag();
-
-                    Intent intent = new Intent(getActivity(), HouseActivity.class);
-                    intent.putExtra("house", house);
-                    startActivity(intent);
-                });
-
-            });
-
         }
-
         getChildFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
 
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(45.46, 9.19), 11));
+            mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter(getActivity()));
+
+            enableMyLocationIfPermitted();
+            mMap.setOnMyLocationButtonClickListener(() -> {
+                mMap.moveCamera(CameraUpdateFactory.zoomBy(13));
+                return false;
+            });
+
+            BitmapDescriptor markerIcon = getBitmapFromVectorDrawable(getContext(), R.drawable.ic_place_black_24dp);
+
+            for (House house : houses) {
+                mMap.addMarker(new MarkerOptions().position(house.getLatLng()).icon(markerIcon)).setTag(house);
+            }
+
+            mMap.setOnInfoWindowClickListener(marker -> {
+                House house = (House) marker.getTag();
+
+                Intent intent = new Intent(getActivity(), HouseActivity.class);
+                intent.putExtra("house", house);
+                startActivity(intent);
+            });
+
+        });
     }
 
     private void enableMyLocationIfPermitted() {
@@ -149,5 +137,4 @@ public class MapFragment extends Fragment{
 
         return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
-
 }
